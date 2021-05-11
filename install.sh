5019b187fc42879c02139561cff399bd544da5ce
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2021-05-11 10:35:00 -0300

    Refactor installation

--- install.sh
+++ install.sh
@@ -2,14 +2,4 @@
 
 set -e
 
-apt-get install libtool-bin
-
-if [[ ! -d /workspaces/neovim ]]; then
-  git clone https://github.com/neovim/neovim /workspaces/neovim
-fi
-
-cd /workspaces/neovim
-
-# compile and install neovim
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-make install
+./install/neovim.sh
